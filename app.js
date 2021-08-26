129fb9090ebcb5b14a0d31d437a60bb5c966fb47
astik kumar <rajveerastik@gmail.com>
2021-08-26 21:20:39 +0530

    ok

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-let a=newdate();
-let time=a.getHours()+':'+ a.getMintues()+':'+a.getSeconds();
+let a;
+let date;
+let time;
+let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+setInterval(() => {
+a= new Date();
+ date=a.toLocaleDateString(undefined,options);
+ time= a.getHours()+':'+a.getMinutes()+ ':'+a.getSeconds();
+document.getElementById('time').innerHTML= time +' <br>on '+date;
+}, 1000);
